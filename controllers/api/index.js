759889235a485560dd3653e64037387a2293681b
jonacko <jessonacko@gmail.com>
2022-10-10 21:33:09 +0100

    amended user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
 
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,6 +19,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
+    console.log(req.body);
     const userData = await User.findOne({ where: { name: req.body.username } });
 
     if (!userData) {
