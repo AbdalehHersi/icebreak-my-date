47e8da0a77eeee5a001d8512d52f406aa0dc0abf
Abdaleh Hersi <therealhersi03@gmail.com>
2022-10-11 21:12:06 +0100

    get response from third party api

--- controllers/api/requests.js
+++ controllers/api/requests.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const axios = require('axios');
 
+// BUCKET LIST
 
 router.get("/bucketlist", async (req, res) => {
   try {
@@ -17,48 +18,39 @@ router.get("/bucketlist", async (req, res) => {
 })
 
 
-// BUCKET LIST
-
-
-router.get({
-  url: 'https://api.api-ninjas.com/v1/bucketlist',
-  headers: {
-    'X-Api-Key': 'cv/3TTGn9rsxmhNJCyXxtw==XZWUQGC4DsfeUJxP'
-  },
-}, function (error, response, body) {
-  if (error) return console.error('Request failed:', error);
-  else if (response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-  else console.log(body)
-});
-
 // HOBBIES
 
-// const request = require('request');
 var category = 'general';
-router.get({
-  url: 'https://api.api-ninjas.com/v1/hobbies?category=' + category,
-  headers: {
-    'X-Api-Key': 'cv/3TTGn9rsxmhNJCyXxtw==XZWUQGC4DsfeUJxP'
-  },
-}, function (error, response, body) {
-  if (error) return console.error('Request failed:', error);
-  else if (response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-  else console.log(body)
-});
 
-// HISTORICAL EVENT  
+router.get("/hobbies", async (req, res) => {
+  try {
+    const response = await axios.get('https://api.api-ninjas.com/v1/hobbies?category=' + category, {
+      headers: {
+        'X-Api-Key': process.env.API_KEY,
+      },
+    })
+    const data = response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+})
 
-// const request = require('request');
-var text = 'roman empire';
-router.get({
-  url: 'https://api.api-ninjas.com/v1/historicalevents?text=' + text,
-  headers: {
-    'X-Api-Key': 'cv/3TTGn9rsxmhNJCyXxtw==XZWUQGC4DsfeUJxP'
-  },
-}, function (error, response, body) {
-  if (error) return console.error('Request failed:', error);
-  else if (response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-  else console.log(body)
-});
+// FACT
+
+router.get("/fact", async (req, res) => {
+  try {
+    const response = await axios.get('https://api.api-ninjas.com/v1/facts?limit=' + 1, {
+      headers: {
+        'X-Api-Key': process.env.API_KEY,
+      },
+    })
+    const data = response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+})
+
 
 module.exports = router;
--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -1,32 +1,4 @@
-
-// // BUCKET LIST
-// function getHobby() {
-
-//   return fetch('https://api.api-ninjas.com/v1/bucketlist', {
-//     headers: {
-//       'X-Api-Key': 'cv/3TTGn9rsxmhNJCyXxtw==XZWUQGC4DsfeUJxP'
-//     },
-//   }).then((res) => {
-//     return res.json()
-//   })
-//     .then((data) => {
-//       console.log(data)
-//     })
-// }
-
-// async function getHobby2() {
-//   try {
-//     const response = await fetch('https://api.api-ninjas.com/v1/bucketlist', {
-//       headers: {
-//         'X-Api-Key': 'cv/3TTGn9rsxmhNJCyXxtw==XZWUQGC4DsfeUJxP'
-//       }
-//     })
-//     const data = await response.json()
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+// BUCKET LIST API
 
 async function getBucket() {
   const url = "/api/request/bucketlist";
@@ -39,8 +11,32 @@ async function getBucket() {
   }
 }
 
+// HOBBIES
 
-const bucketButton = document.getElementById('btn-bucket');
-console.log("BUCKET BUTTON PRESSED", bucketButton);
-bucketButton.addEventListener('click', getBucket);
-// document.getElementById('btn-hobby').addEventListener('click', getHobby);
+async function getHobby() {
+  const url = "/api/request/hobbies";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// HISTORICAL EVENTS
+
+async function getFact() {
+  const url = "/api/request/fact";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data[0])
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+document.getElementById('btn-hobby').addEventListener('click', getHobby);
+document.getElementById('btn-fact').addEventListener('click', getFact);
+document.getElementById('btn-bucket').addEventListener('click', getBucket);
