ab6812615761ec7bc4bcadd0e984f664185ec277
jonacko <jessonacko@gmail.com>
2022-10-11 17:05:15 +0100

    added login function, logout function, get hobby API call

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -20,7 +20,7 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     console.log(req.body);
-    const userData = await User.findOne({ where: { name: req.body.username } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
